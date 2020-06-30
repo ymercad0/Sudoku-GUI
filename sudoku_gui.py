89b9951ae01f9f6bea66ba12d180373acc73f827
Yariel <yariel.then@gmail.com>
2020-06-30 09:25:58 -0500

    Backup current code

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -24,32 +24,30 @@ class Board:
         for i in range(9):
             for j in range(9):
                 if j%3 == 0 and j != 0: #vertical lines
-                    pygame.draw.line(self.window, (0, 0, 0), (((j//3)*180)+1, 0), (((j//3)*180)+1, 540), 5)
+                    pygame.draw.line(self.window, (0, 0, 0), ((j//3)*180, 0), ((j//3)*180, 540), 4)
                     pygame.display.flip()
 
                 if i%3 == 0 and i != 0: #horizontal lines
-                    pygame.draw.line(self.window, (0, 0, 0), (0, ((i//3)*180)+1), (540, ((i//3)*180)+1), 5)
+                    pygame.draw.line(self.window, (0, 0, 0), (0, (i//3)*180), (540, (i//3)*180), 4)
                     pygame.display.flip()
 
                 self.tiles[i][j] = Tile(self.board.get_board()[i][j], self.window, i*60, j*60) #each tile has a distance of 60 units to the other
-                self.tiles[i][j].draw()
+                self.tiles[i][j].draw((0,0,0), 1)
                 if self.board.get_board()[i][j] != 0: #don't draw 0s on the grid
                     self.tiles[i][j].display((20+(j*60), (5+(i*60))))  #20,5 are the coordinates of the first tile
+        return self.tiles
 
 class Tile:
     '''Represents each white tile/box on the grid'''
     def __init__(self, value, window, x1, x2):
         self.value = value #value of the num on this grid
-        self.rows = 9
-        self.cols = 9
-        self.width = 60
-        self.height = 60
-        self.window = window #the window/screen we're in
-        self.rect = pygame.Rect(x1, x2, self.width, self.height) #dimensions for the rectangle
+        self.window = window
+        self.active = False
+        self.rect = pygame.Rect(x1, x2, 60, 60) #dimensions for the rectangle
 
-    def draw(self):
+    def draw(self, color, thickness):
         '''Draws a tile on the board'''
-        pygame.draw.rect(self.window, (0,0,0), self.rect, 1)
+        pygame.draw.rect(self.window, color, self.rect, thickness)
         pygame.display.flip()
 
     def display(self, position):
@@ -59,20 +57,34 @@ class Tile:
         self.window.blit(text, position)
         pygame.display.flip()
 
+    def is_clicked(self, mousePos):
+        '''Checks if a tile has been clicked'''
+        return self.rect.collidepoint(mousePos) #checks if a user has clicked inside of the Rect coords
+
 def main():
     '''Runs the main Sudoku GUI/Game'''
-    screen = pygame.display.set_mode((540, 540))  #make a screen
+    screen = pygame.display.set_mode((540, 540))
     screen.fill((255, 255, 255))
     pygame.display.set_caption("Sudoku Solver")
     icon = pygame.image.load("icon.png")
     pygame.display.set_icon(icon)
 
-    y = Board(screen)
-    y.draw_board()
+    board = Board(screen)
+    tiles = board.draw_board() #store the locations of all the tiles on the grid
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+            elif event.type == pygame.MOUSEBUTTONUP:
+                mousePos = pygame.mouse.get_pos()
+                for i in range(9):
+                    for j in range (9): #look for tile we clicked on
+                        tiles[i][j].draw((0,0,0),1)
+                        if tiles[i][j].is_clicked(mousePos):
+                            tiles[i][j].draw((50,205,50),4) #redraws that tile but with a highlighted color to show it's been clicked
+                            break
+
 main()
