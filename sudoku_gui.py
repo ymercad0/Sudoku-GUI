460d02b8568d430b0fe77fd18b84622904224cf0
Yariel <yariel.then@gmail.com>
2020-07-06 17:02:16 -0500

    Fixed more board bugs

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -16,7 +16,7 @@ class Board:
             [0, 0, 0, 4, 1, 9, 0, 0, 5],
             [0, 0, 0, 0, 8, 0, 0, 7, 9]
         ])
-        self.tiles = [[Tile(self.board.get_board()[i][j], window, i*60, j*60) for i in range(9)] for j in range(9)]
+        self.tiles = [[Tile(self.board.get_board()[i][j], window, i*60, j*60) for j in range(9)] for i in range(9)]
         self.window = window
 
     def draw_board(self):
@@ -31,16 +31,15 @@ class Board:
 
                 self.tiles[i][j].draw((0,0,0), 1)
 
-                if self.board.get_board()[i][j] != 0: #don't draw 0s on the grid
+                if self.tiles[i][j].value != 0: #don't draw 0s on the grid
                     self.tiles[i][j].display((20+(j*60), (5+(i*60))))  #20,5 are the coordinates of the first tile
-        return self.tiles
 
     def deselect(self, tile):
         '''Deselects every tile except the one currently clicked'''
         for i in range(9):
             for j in range(9):
                 if self.tiles[i][j] != tile:
-                    self.tiles[i][j].undo_select()
+                    self.tiles[i][j].selected = False
 
     def redraw(self):
         '''Redraws board with highlighted tiles'''
@@ -48,8 +47,8 @@ class Board:
         self.draw_board()
         for i in range(9):
             for j in range(9):
-                if self.tiles[i][j].is_selected():  # draws the green border on selected tiles
-                    self.tiles[i][j].draw((50, 205, 50), 5)
+                if self.tiles[i][j].selected:  # draws the green border on selected tiles
+                    self.tiles[i][j].draw((50, 205, 50), 4)
 
 class Tile:
     '''Represents each white tile/box on the grid'''
@@ -75,13 +74,6 @@ class Tile:
             self.selected = True
         return self.selected
 
-    def undo_select(self):
-        ''''Resets an unselected tile'''
-        self.selected = False
-
-    def is_selected(self):
-        return self.selected
-
 def main():
     '''Runs the main Sudoku GUI/Game'''
     screen = pygame.display.set_mode((540, 540))
@@ -91,7 +83,7 @@ def main():
     pygame.display.set_icon(icon)
 
     board = Board(screen)
-    tiles = board.draw_board()
+    board.draw_board()
 
     running = True
     while running:
@@ -103,8 +95,8 @@ def main():
                 mousePos = pygame.mouse.get_pos()
                 for i in range(9):
                     for j in range (9):
-                        if tiles[i][j].clicked(mousePos):
-                            board.deselect(tiles[i][j]) #deselects every tile except the one currently clicked
+                        if board.tiles[i][j].clicked(mousePos):
+                            board.deselect(board.tiles[i][j]) #deselects every tile except the one currently clicked
 
         board.redraw()
         pygame.display.flip()
