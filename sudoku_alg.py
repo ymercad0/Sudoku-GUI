e19563ebdbc9447b218916cffeca76aeeb9aa511
Yariel <yariel.then@gmail.com>
2020-07-25 15:09:36 -0500

    Deleted unecessary class

--- sudoku_alg.py
+++ sudoku_alg.py
@@ -1,100 +1,70 @@
-import random
-from copy import deepcopy
-def generate():
-    '''Randomly generates a Sudoku grid/board'''
-    while True:  #return will interrupt the loop
-        board = Sudoku([ #recursion is really slow and inneficient
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        ])
-        # puts one random number, then solves the board to generate a board
-        for i in range(9):
-            for j in range(9):
-                if random.randint(1, 10) >= 5:
-                    board.board[i][j] = random.randint(1, 9)  #plug in random number at random spot
-                    if board.valid((i, j), board.board[i][j]):
-                        continue
-                    else:
-                        board.board[i][j] = 0
-
-        partialBoard = deepcopy(board) #copies board without being modified after .solve is called
-        if board.solve():
-            return partialBoard
-
-class Sudoku:
-    '''Initialize a board represented by ints as a 2D Array'''
-    def __init__ (self, board):
-        '''Initialize the board'''
-        self.board = board
-
-    def print_board(self):
-        '''Prints the board'''
-        boardString = ""
-        for i in range(9):
-            for j in range(9):
-                boardString += str(self.board[i][j]) + " "
-                if (j+1)%3 == 0 and j != 0 and (j+1) != 9:
-                    boardString += "| "
-
-                if j == 8:
-                    boardString += "\n"
-
-                if j == 8 and (i+1)%3 == 0 and (i+1) != 9:
-                    boardString += "- - - - - - - - - - - \n"
-        print(boardString)
-
-    def find_empty (self):
-        '''Finds an empty cell and returns its position as a tuple'''
-        for i in range (9):
-            for j in range (9):
-                if self.board[i][j] == 0:
-                    return i,j
-
-    def valid(self, pos, num):
-        '''Whether a number is valid in that cell, returns a bool'''
-        for i in range(9):
-            if self.board[i][pos[1]] == num and (i, pos[1]) != pos:  #make sure it isn't the same number we're checking for by comparing coords
-                return False
-
+def print_board(board):
+    '''Prints the board'''
+    boardString = ""
+    for i in range(9):
         for j in range(9):
-            if self.board[pos[0]][j] == num and (pos[0], j) != pos:  #Same row but not same number
-                return False
+            boardString += str(board[i][j]) + " "
+            if (j+1)%3 == 0 and j != 0 and (j+1) != 9:
+                boardString += "| "
 
-        start_i = pos[0] - pos[0] % 3 #ex. 5-5%3 = 3 and thats where the grid starts
-        start_j = pos[1] - pos[1] % 3
-        for i in range(3):
-            for j in range(3):  #adds i and j as needed to go from start of grid to where we need to be
-                if self.board[start_i + i][start_j + j] == num and (start_i + i, start_j + j) != pos:
-                    return False
+            if j == 8:
+                boardString += "\n"
+
+            if j == 8 and (i+1)%3 == 0 and (i+1) != 9:
+                boardString += "- - - - - - - - - - - \n"
+    print(boardString)
+
+def find_empty (board):
+    '''Finds an empty cell and returns its position as a tuple'''
+    for i in range (9):
+        for j in range (9):
+            if board[i][j] == 0:
+                return i,j
+
+def valid(board, pos, num):
+    '''Whether a number is valid in that cell, returns a bool'''
+    for i in range(9):
+        if board[i][pos[1]] == num and (i, pos[1]) != pos:  #make sure it isn't the same number we're checking for by comparing coords
+            return False
+
+    for j in range(9):
+        if board[pos[0]][j] == num and (pos[0], j) != pos:  #Same row but not same number
+            return False
+
+    start_i = pos[0] - pos[0] % 3 #ex. 5-5%3 = 3 and thats where the grid starts
+    start_j = pos[1] - pos[1] % 3
+    for i in range(3):
+        for j in range(3):  #adds i and j as needed to go from start of grid to where we need to be
+            if board[start_i + i][start_j + j] == num and (start_i + i, start_j + j) != pos:
+                return False
+    return True
+
+def solve(board):
+    '''Solves the Sudoku board via the backtracking algorithm'''
+    empty = find_empty(board)
+    if not empty: #no empty spots are left so the board is solved
         return True
 
-    def get_board(self):
-        '''Returns the board not as an instance of Sudoku but as a 2D array'''
-        return self.board
+    for nums in range(9):
+        if valid(board, empty,nums+1):
+            board[empty[0]][empty[1]] = nums+1
 
-    def solve(self):
-        '''Solves the Sudoku board via the backtracking algorithm'''
-        empty = self.find_empty()
-        if not empty: #no empty spots are left so the board is solved
-            return True
-
-        for nums in range(9):
-            if self.valid(empty,nums+1):
-                self.board[empty[0]][empty[1]] = nums+1
-
-                if self.solve(): #recursive step
-                    return True
-                self.board[empty[0]][empty[1]] = 0 #this number is wrong so we set it back to 0
-        return False
+            if solve(board): #recursive step
+                return True
+            board[empty[0]][empty[1]] = 0 #this number is wrong so we set it back to 0
+    return False
 
 if __name__ == '__main__':
-    board = generate()
-    board.solve()
-    board.print_board()
+    board =  [
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
+    solve(board)
+    print_board(board)
